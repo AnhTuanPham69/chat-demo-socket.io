c774fd65442c6cf124bf856e482780dd5bc81160
AnhTuanPham69 <anhtuanpham.dev@gmail.com>
2022-01-04 22:56:51 +0700

    ok

---
+++ client/src/App.js
@@ -0,0 +1,162 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+import io from "socket.io-client";
+
+const Page = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  align-items: center;
+  background-color: #fffff;
+  flex-direction: column;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+  max-height: 500px;
+  overflow: auto;
+  width: 400px;
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  padding-bottom: 10px;
+  margin-top: 25px;
+`;
+
+const TextArea = styled.textarea`
+  width: 98%;
+  height: 50px;
+  border-radius: 15px;
+  margin-top: 10px;
+  padding-left: 10px;
+  padding-top: 10px;
+  font-size: 17px;
+  background-color: transparent;
+  border: 1px solid lightgray;
+  outline: none;
+  color: black;
+  letter-spacing: 1px;
+  line-height: 20px;
+  ::placeholder {
+    color: lightgray;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #0084ff;
+  width: 100%;
+  border: none;
+  height: 50px;
+  border-radius: 25px;
+  color: #ffff;
+  font-size: 17px;
+`;
+
+const Form = styled.form`
+  width: 400px;
+`;
+
+const MyRow = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 10px;
+`;
+
+const MyMessage = styled.div`
+  width: 45%;
+  background-color: #0084ff;
+  color: white;
+  padding: 10px;
+  margin-right: 5px;
+  text-align: center;
+  border-radius: 25px;
+`;
+
+const PartnerRow = styled(MyRow)`
+  justify-content: flex-start;
+`;
+
+const PartnerMessage = styled.div`
+  width: 45%;
+  background-color: rgb(228 230 235);
+  color: black;
+  border: 1px solid rgb(228 230 235);
+  padding: 10px;
+  margin-left: 5px;
+  text-align: center;
+  border-top-right-radius: 20px;
+  border-bottom-right-radius: 20px;
+  border-top-left-radius: 20px;
+`;
+
+const App = () => {
+  const [yourID, setYourID] = useState();
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const socketRef = useRef();
+
+  useEffect(() => {
+    socketRef.current = io.connect('/');
+
+    socketRef.current.on("your id", id => {
+      setYourID(id);
+    })
+
+    socketRef.current.on("message", (message) => {
+      console.log("here");
+      receivedMessage(message);
+    })
+  }, []);
+
+  function receivedMessage(message) {
+    setMessages(oldMsgs => [...oldMsgs, message]);
+  }
+
+  function sendMessage(e) {
+    e.preventDefault();
+    const messageObject = {
+      body: message,
+      id: yourID,
+    };
+    setMessage("");
+    socketRef.current.emit("send message", messageObject);
+  }
+
+  function handleChange(e) {
+    setMessage(e.target.value);
+  }
+
+  return (
+    <Page>
+      <Container>
+        {messages.map((message, index) => {
+          if (message.id === yourID) {
+            return (
+              <MyRow key={index}>
+                <MyMessage>
+                  {message.body}
+                </MyMessage>
+              </MyRow>
+            )
+          }
+          return (
+            <PartnerRow key={index}>
+              <PartnerMessage>
+                {message.body}
+              </PartnerMessage>
+            </PartnerRow>
+          )
+        })}
+      </Container>
+      <Form onSubmit={sendMessage}>
+        <TextArea value={message} onChange={handleChange} placeholder="Say something..." />
+        <Button>Send</Button>
+      </Form>
+    </Page>
+  );
+};
+
+export default App;
